2241e6862772f14a8494cc6e527fd82315f287e9
Abdulwahab Murshed <73560945+fgghvyuy@users.noreply.github.com>
2022-03-28 18:12:56 -0400

    Day 1 finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,37 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  var questionNum = 1;
-  console.log(data[questionNum].question.text);
+  var questionNum = 0;
+  let [AnswerIsShown, setAnswerIsShown] = useState(false);
   return (
     <div className="app">
       Trivia!
-      <Question question={data[questionNum].question.text} />
+      <Question
+        question={data[questionNum].question.text}
+        answers={data[questionNum].question.choices}
+      />
+      <NextQuestion />s
     </div>
   );
 }
+function Answer(props) {
+  return <p>{props.choice}</p>;
+}
+
 function Question(props) {
-  return <h1 className="question"> {props.question}</h1>;
+  let AnswerParagraphs = props.answers.map((choice) => (
+    <Answer choice={choice} />
+  ));
+  return (
+    <div>
+      <h1 className="question"> {props.question}</h1>
+      {AnswerParagraphs}
+    </div>
+  );
+}
+
+function NextQuestion() {
+  return <button> Next Question </button>;
 }
 
 export default App;
